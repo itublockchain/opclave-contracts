c015cf489f0f58b26083028f1854148d0a38ee07
tahos81 <tahirozpala@gmail.com>
2023-03-26 05:22:22 +0300

    factory update

--- src/Factory.sol
+++ src/Factory.sol
@@ -2,26 +2,8 @@
 pragma solidity ^0.8.12;
 
 contract Factory {
-    function create(bytes calldata initializationCode) public returns (address deploymentAddress) {
+    function create(uint256 salt, bytes calldata initializationCode) public returns (address deploymentAddress) {
         bytes memory initCode = initializationCode;
-        uint256 salt = 0;
-
-        // determine the target address for contract deployment.
-        address targetDeploymentAddress = address(
-            uint160( // downcast to match the address type.
-                uint256( // convert to uint to truncate upper digits.
-                    keccak256( // compute the CREATE2 hash using 4 inputs.
-                        abi.encodePacked( // pack all inputs to the hash together.
-                            hex"ff", // start with 0xff to distinguish from RLP.
-                            address(this), // this contract will be the caller.
-                            salt, // pass in the supplied salt value.
-                            keccak256( // pass in the hash of initialization code.
-                            abi.encodePacked(initCode))
-                        )
-                    )
-                )
-            )
-        );
 
         // using inline assembly: load data and length of data, then call CREATE2.
         assembly {
@@ -36,11 +18,28 @@ contract Factory {
                     salt // pass in the salt value.
                 )
         }
+    }
 
-        // check address against target to ensure that deployment was successful.
-        require(
-            deploymentAddress == targetDeploymentAddress,
-            "Failed to deploy contract using provided salt and initialization code."
+    function findCreate2Address(uint256 salt, bytes calldata initCode)
+        external
+        view
+        returns (address deploymentAddress)
+    {
+        // determine the address where the contract will be deployed.
+        deploymentAddress = address(
+            uint160( // downcast to match the address type.
+                uint256( // convert to uint to truncate upper digits.
+                    keccak256( // compute the CREATE2 hash using 4 inputs.
+                        abi.encodePacked( // pack all inputs to the hash together.
+                            hex"ff", // start with 0xff to distinguish from RLP.
+                            address(this), // this contract will be the caller.
+                            salt, // pass in the supplied salt value.
+                            keccak256( // pass in the hash of initialization code.
+                            abi.encodePacked(initCode))
+                        )
+                    )
+                )
+            )
         );
     }
 }
